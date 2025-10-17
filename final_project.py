10bb02ed71896cf677a15e73a9b33d88ac181516
Home <home@Mac.lan>
2025-10-17 03:01:32 -0400

    Final draft

--- final_project.py
+++ final_project.py
@@ -1,36 +1,67 @@
 # final project code for nuclear physics 1 with Dr. Cates
 # My method of solving was ODEs and numerical solutions.
-import mylib
+
+# import necessary modules
+import mylib # I made this!
 import numpy as np
+import matplotlib.pyplot as plt
+import math
 
 # This model depends primarily on the size of the bomb. This in turn determines the rate at which neutrons leave the spherical bomb.
 
 # Find ejection probabilities based on radius and mfp
-radii_to_test = [1, 5, 10, 15, 20]
-mean_free_path = 0.1
+radii_to_test = [10, 15, 20, 25, 30]
+uranium_density = 4.8*10**(22) # /cm^3
+uranium_cross_section = 1.2 * 10**(-24) # cm^2
+
+mean_free_path = -1/(uranium_cross_section * uranium_density) * math.log(1/2)
+# print(mean_free_path)
+
+
+fission_probability = math.exp(-uranium_cross_section * uranium_density * mean_free_path)
+# print(fission_probability)
 probability_to_eject = mylib.calculate_ejection_probabilities(radii_to_test, mean_free_path)
 
 print('radii', radii_to_test)
-print('probs', probability_to_eject)
+print('fission probability', fission_probability)
+print('eject probability', probability_to_eject)
 
 # Test these ejection probabilities to see what happens
 # Look at just 100 iterations of Euler Method ODE Solver
 
 time_span = 10**(-6) # 1 µs
 time_step = 10 * 10**(-9) # 10 ns
-n_neutrons = 100
-n_generations = 10
+n_neutrons = 1
+n_generations = 20
 
 initial_state = [n_neutrons] # 100 starter neutrons
-for i in range(n_generations): # make room for 10 generations if needed.
+for i in range(n_generations - 1): # make room for more generations if needed.
     initial_state.append(0)
-for prob in probability_to_eject:
-    test_state = initial_state
-    time, Generations, Exit = mylib.euler_chain_solver(test_state, 0, .1, prob, time_span, time_step)
-    print('got data for prob:', prob)
-    for i in range(99, 100):
-        print('printing for prob:', prob)
-        print(Generations[i].round(), Exit[i].round())
-        print(round(sum(Generations[i])), round(Exit[i]))
 
-# Let's examine k_eff for each of these generations. k_eff is neutrons in current generation divided by neutrons in previous generation.
+# now test!
+for rad, prob in enumerate(probability_to_eject):
+    test_state = initial_state # just to keep a backup.
+    time, Generations, Exit = mylib.euler_chain_solver(test_state, 0, fission_probability, prob, time_span, time_step)
+    old_list = []
+    older_list = []
+    oldest_list = []
+    for i in range(50, 100):
+        new_list = []
+        newer_list = []
+        newest_list = []
+        new_list.append(Generations[i][2] / Generations[i][1])
+        newer_list.append(Generations[i][4] / Generations[i][3])
+        newest_list.append(Generations[i][8] / Generations[i][7])
+        old_list.append(sum(new_list) / len(new_list))
+        older_list.append(sum(newer_list) / len(newer_list))
+        oldest_list.append(sum(newest_list) / len(newest_list))
+    plt.plot(old_list, linestyle = '-', label = 'between generations 2 and 1')
+    plt.plot(older_list, linestyle = ':', label = '4 and 3')
+    plt.plot(oldest_list, linestyle = '--', label = ' 8 and 7')
+    plt.xlabel('time after 50 * 10 ns in ns')
+    plt.ylabel('average k_eff of first half of generations')
+    plt.title('k_eff at radius ' + str(radii_to_test[rad]) + ' cm')
+    plt.grid()
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
