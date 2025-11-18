633a9ba37fe56913575414f8b6b3c1239054aafd
Home <home@Homes-MacBook-Air.local>
2025-11-17 22:56:20 -0500

    Edited for clarity as final draft.

--- library.py
+++ library.py
@@ -2,16 +2,6 @@ import numpy as np
 import math
 from numba import njit
 
-# @njit
-def k_number():
-    """
-    Given that fission occured, determines number of product neutrons.
-    Either create 2 or 3 new neutrons.
-    Returns :
-    int: random int between 2 and 3
-    """
-    return int(np.random.choice((2,3)))
-
 @njit(fastmath = True, cache = True)
 def mc_integrator3(N, radius, delta_size):
     """
@@ -87,60 +77,3 @@ def simple_bomb_odes(y, t, fission_rate, ejection_rate):
     dRdt = ejection_rate * (I)
     dPdt = fission_rate * S * I #counting the fissions, not neutrons produced!
     return [dSdt, dIdt, dRdt, dPdt]
-
-def chain_odes(state, fission_rate, ejection_rate):
-    """
-    state: array of length N+1 -> [G1, G2, ..., GN, E]
-    fission_rate: transition rate for generations
-    ejection_rate: loss rate from each Gi into E
-    """
-    N = len(state) - 1
-    G = state[:N] # stands for generations
-    E = state[-1] # stands for exited neutrons
-
-    dG = np.empty(N, dtype=float)
-
-    # G1
-    dG[0] = -(fission_rate + ejection_rate) * G[0]
-
-    # G2..GN
-    for i in range(1, N):
-        dG[i] = (fission_rate * k_number() * G[i - 1] - 
-                 (fission_rate + ejection_rate) * G[i])
-
-    # E
-    dE = ejection_rate * np.sum(G)
-
-    return np.concatenate([dG, [dE]])
-
-def euler_chain_solver(initial_conditions_minus_E, 
-                       initial_conditions_only_E, 
-                       fission_prob, 
-                       ejection_prob, 
-                       total_time, 
-                       dt):
-    """
-    G0: array-like length N (initial values for G1..GN)
-    E0: float (initial E)
-    returns: t (T,), G (T,N), E (T,)
-    """
-    initial_conditions = np.asarray(initial_conditions_minus_E, dtype=float)
-    N = len(initial_conditions)
-
-    t = np.arange(0.0, total_time + dt, dt)
-    T = len(t)
-
-    Y = np.zeros((T, N + 1), dtype=float)
-    Y[0, :N] = initial_conditions
-    Y[0, -1] = initial_conditions_only_E
-
-    fission_rate = fission_prob / dt
-    ejection_rate = ejection_prob / dt
-
-    for i in range(T - 1):
-        dY = chain_odes(Y[i], fission_rate, ejection_rate)
-        Y[i + 1] = Y[i] + dt * dY
-
-    G = Y[:, :N]
-    E = Y[:, -1]
-    return t, G, E
