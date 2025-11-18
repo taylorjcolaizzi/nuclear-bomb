4fcdd19e74efabf7345e94dadc159906313e06a0
Home <home@Homes-MacBook-Air.local>
2025-11-17 22:57:05 -0500

    Deleted old files.

--- final_project.py
+++ final_project.py
@@ -1,102 +1,62 @@
 # final project code for nuclear physics 1 with Dr. Cates
-# My method of solving was ODEs and numerical integration.
+# My method of solving was ODEs and monte carlo integration.
 
 import library # I made this!
-import matplotlib.pyplot as plt
-import math
-import scipy
+import matplotlib.pyplot as plt # graphing
+import math # log, exp
+import scipy # ODE integrator
+import numpy # various linear algebra tools
 
 # This model depends primarily on the size of the bomb. 
 # This in turn determines the rate at which neutrons leave the spherical bomb.
+# You may also specify the cross sections and the density, but these I've left
+# as the default values for 1 MeV neutrons hitting U235
 
-# Find ejection probabilities based on radius and mfp
-radii_to_test = [i for i in range(1,5, 5)] # list generation
-uranium_density = 4.8 * (10**(22)) # 1/cm^3
-uranium_cross_section = 1.2 * (10**(-24)) # cm^2
+# Find ejection probabilities based on radius and mean free path
+radii_to_test = numpy.linspace(2, 12, 3) # it explodes quickly!
+volume_to_test = [4/3 * math.pi * r**3 for r in radii_to_test]
 
+URANIUM_DENSITY = 4.8 * (10**(22)) # 1/cm^3
 FISSION_CROSS_SECTION = 1.2 * (10**(-24)) # cm^2 at 1 MeV
-ELASTIC_CROSS_SECTION = 3.6 * (10**(-24)) # cm^2 at 1 MeV
+ELASTIC_CROSS_SECTION = 6.7 * (10**(-24)) # cm^2 at 1 MeV
 
 # mean_free_path is average length before fission
-mean_free_path = -1/(uranium_cross_section * uranium_density) * math.log(1/2)
-print(mean_free_path)
+fission_mean_free_path = -1/(FISSION_CROSS_SECTION * URANIUM_DENSITY) * math.log(1/2)
+elastic_mean_free_path = -1/(ELASTIC_CROSS_SECTION * URANIUM_DENSITY) * math.log(1/2)
+scattering_centers = [(r / elastic_mean_free_path)**2 * 185 for r in radii_to_test]
+# scale number of scattering_centers to number of neutrons released by neutron
+# source. That way, we can just have 1 neutron in the initial population later on.
 
 fission_probability = math.exp(
-    -uranium_cross_section * uranium_density * mean_free_path
+    -FISSION_CROSS_SECTION * URANIUM_DENSITY * fission_mean_free_path
     )
-print(fission_probability)
+elastic_probability = math.exp(
+    -ELASTIC_CROSS_SECTION * URANIUM_DENSITY * elastic_mean_free_path
+    )
 
-probability_to_eject = library.calculate_ejection_probabilities(radii_to_test, 
-                                                                mean_free_path)
+probability_to_eject = library.calculate_ejection_probabilities(
+    radii_to_test, 
+    elastic_mean_free_path
+    )
 
-print('radii', radii_to_test)
-print('fission probability', fission_probability)
-print('eject probability', probability_to_eject)
-
-# Test these ejection probabilities to see what happens
-# Look at just 100 iterations of Euler Method ODE Solver
-
-time_span = 10**(-6) # 1 µs
-time_step = 10 * (10**(-9)) # 10 ns
-n_neutrons = 1 * 1000 * 1 # start with 1000
-n_generations = 20 # go up to 20 generations
-
-initial_state = [n_neutrons] # starter neutrons
-for i in range(n_generations - 1): # make room for more generations if needed.
-    initial_state.append(0) # total length is n_generations
-
-# now test!
-for radius, probability in enumerate(probability_to_eject):
-    test_state = initial_state # just to keep a backup.
-    time, Generations, Exit = library.euler_chain_solver(test_state, 
-                                                         0, 
-                                                         fission_probability, 
-                                                         probability, 
-                                                         time_span, 
-                                                         time_step)
-    old_list = []
-    older_list = []
-    oldest_list = []
-    for i in range(50, 100):
-        new_list = []
-        newer_list = []
-        newest_list = []
-        new_list.append(Generations[i][2] / Generations[i][1])
-        newer_list.append(Generations[i][4] / Generations[i][3])
-        newest_list.append(Generations[i][8] / Generations[i][7])
-        old_list.append(sum(new_list) / len(new_list))
-        older_list.append(sum(newer_list) / len(newer_list))
-        oldest_list.append(sum(newest_list) / len(newest_list))
-    plt.plot(old_list, linestyle = '-', label = 'between generations 2 and 1')
-    plt.plot(older_list, linestyle = ':', label = '4 and 3')
-    plt.plot(oldest_list, linestyle = '--', label = ' 8 and 7')
-    plt.xlabel('time after 50 * 10 ns in ns')
-    plt.ylabel('average k_eff of first half of generations')
-    plt.title('k_eff at radius ' + str(radii_to_test[radius]) + ' cm')
-    plt.grid()
-    plt.legend()
-    plt.tight_layout()
-    # plt.show()
-
-# now, let's do the same test but with the simple ODE method!
-# uses "real_bomb_odes" function
+TIME_SPAN = 1e-6 # 1 µs
+TIME_STEP = 10e-9 # 10 ns
+N_NEUTRONS = 1. # already multiplied by 185 earlier on.
 
 # using the same method as the SIR_Example, but we have a more complicated state
 
 # Initial conditions
-N = 1000
-I0 = 1
-R0 = 0
-P0 = 0
-S0 = N - I0 - R0 - P0
+I0 = N_NEUTRONS
+R0 = 0.
+P0 = 0.
+S0 = scattering_centers[0]
+N = 1000.
 
-fission_rate = - math.log(1 - 0.5) / (10e-9)
-print(fission_rate)
-ejection_rate = - math.log(1 - 0.7) / (10e-9)
+fission_rate = - math.log(1 - fission_probability) / (TIME_STEP)
 
-import numpy
-t = numpy.linspace(0, 1e-6, 101)
-print(t)
+ejection_rate = - math.log(1e-10 if probability_to_eject[0] == 1 else 1 - probability_to_eject[0]) / (TIME_STEP)
+
+t = numpy.linspace(0, TIME_SPAN, int(TIME_SPAN/TIME_STEP + 1))
 
 y0 = [S0/N, I0/N, R0/N, P0/N]
 
@@ -108,17 +68,33 @@ solution = scipy.integrate.odeint(
     )
 S, I, R, P = solution.T
 
-plt.figure(figsize=(10,6))
-plt.plot(t, S, label='Fission Centers', linestyle = '-', linewidth = 3)
-plt.plot(t, I, label='Free Neutrons', linestyle = ':', linewidth = 3)
-plt.plot(t, R, label='Escaped', linestyle = '--', linewidth = 3)
-plt.plot(t, P, label='Total Fissions', linestyle = '-.', linewidth = 3)
-plt.xlabel('Time (days)', size = 16)
-plt.ylabel('Fraction of Population', size = 16)
-plt.title('Example SIR Model with beta = 0.3 and gamma = 0.1', size = 24)
-plt.legend(fontsize = 16)
-plt.grid(True)
-plt.tight_layout()
+for i, r in enumerate(radii_to_test):
+    S0 = scattering_centers[i]
+    ejection_rate = - math.log(1e-10 if probability_to_eject[i] == 1 else 1-probability_to_eject[i])/TIME_STEP
 
-plt.savefig('really_trying.jpg')
-plt.show()
+    y0 = [S0/S0, I0/S0, R0/S0, P0/S0]
+
+    solution = scipy.integrate.odeint(
+        library.simple_bomb_odes,
+        y0,
+        t,
+        args = (fission_rate, ejection_rate)
+    )
+    S, I, R, P = solution.T
+
+    plt.figure(figsize=(10,6))
+    plt.plot(t, S, label='Fission Centers', linestyle = '-', linewidth = 3)
+    plt.plot(t, I, label='Free Neutrons', linestyle = ':', linewidth = 3)
+    plt.plot(t, R, label='Escaped', linestyle = '--', linewidth = 3)
+    plt.plot(t, P, label='Total Fissions', linestyle = '-.', linewidth = 3)
+    plt.xlabel('Time (10 ns step size)', size = 16)
+    plt.ylabel("Fraction of Population", size = 16)
+    plt.title(f'radius = {round(r, 1)} cm and final total fissions = {round(P[-1]*S0*185, 0)}', size = 24)
+    plt.legend(fontsize = 16)
+    plt.grid(True)
+    plt.tight_layout()
+
+    # As the plots print, go ahead and save the ones you like.
+
+    plt.savefig('temp_plot.jpg')
+    plt.show()
